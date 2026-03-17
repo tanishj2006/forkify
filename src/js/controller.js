2c95459bac7717f04e8d77565d493b0c7805cc5e
Tanish Jain <tanishj52@gmail.com>
2026-03-17 23:40:07 +0530

    feat: fetch recipe data from Forkify API and log result

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,5 +9,30 @@ const timeout = function (s) {
 };
 
 // NEW API URL (instead of the one shown in the video)
-// https://forkify-api.jonas.io
-console.log('Test');
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    // console.log(res, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      serving: recipe.serving,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
