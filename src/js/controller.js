9a04d9e8f5a39de03096088edb3cc1c0c6eaf85c
Tanish Jain <tanishj52@gmail.com>
2026-03-20 16:27:57 +0530

    feat: handle recipe loading on hashchange and page load

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,9 +29,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -155,4 +159,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+// showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
