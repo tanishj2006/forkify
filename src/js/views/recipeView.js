b56bd1d6d909114a5aa0fd491e7e07a59f48160c
Tanish Jain <tanishj52@gmail.com>
2026-03-21 17:56:49 +0530

    feat: format ingredient quantities using fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,6 @@
 import icons from 'url:../../img/icons.svg';
+import fracty from 'fracty';
+console.log(fracty);
 
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
@@ -87,20 +89,7 @@ class RecipeView {
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
             ${this.#data.ingredients
-              .map(ing => {
-                return `
-                <li class="recipe__ingredient">
-                  <svg class="recipe__icon">
-                    <use href="${icons}#icon-check"></use>
-                  </svg>
-                  <div class="recipe__quantity">${ing.quantity}</div>
-                  <div class="recipe__description">
-                    <span class="recipe__unit">${ing.unit}</span>
-                    ${ing.description}
-                  </div>
-                </li>
-              `;
-              })
+              .map(this.#generateMarkupIngredient)
               .join('')}
           </ul>
         </div>
@@ -126,6 +115,23 @@ class RecipeView {
           </a>
         </div>
     `;
+  }
+
+  #generateMarkupIngredient(ing) {
+    return `
+        <li class="recipe__ingredient">
+          <svg class="recipe__icon">
+            <use href="${icons}#icon-check"></use>
+          </svg>
+          <div class="recipe__quantity">${
+            ing.quantity ? fracty(ing.quantity) : ''
+          }</div>
+          <div class="recipe__description">
+            <span class="recipe__unit">${ing.unit}</span>
+            ${ing.description}
+          </div>
+        </li>
+      `;
   }
 }
 
