f4771d49a0ba3e93271d947c4ef4d1cfca9d9321
Tanish Jain <tanishj52@gmail.com>
2026-03-22 15:23:15 +0530

    refactor: implement publisher-subscriber pattern with addHandlerRender

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,12 +19,12 @@ const controlRecipes = async function () {
     // 2 Render Recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-// controlRecipes();
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-// window.addEventListener('hashchange', controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
