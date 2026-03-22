c3f7912394dd3f01d1e94e54a6d868156b4abe3d
Tanish Jain <tanishj52@gmail.com>
2026-03-22 15:04:34 +0530

    chore: add helpers module for reusable utility functions

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,5 @@
 import { API_URL } from './config';
+import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
@@ -6,10 +7,8 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(`${API_URL}/${id}`);
-    const data = await res.json();
+    const data = await getJSON(`${API_URL}/${id}`);
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -23,6 +22,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} 🔴`);
   }
 };
