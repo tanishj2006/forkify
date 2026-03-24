412768f46ec4fab38ad7968054ed79025bd47a2d
Tanish Jain <tanishj52@gmail.com>
2026-03-24 17:13:01 +0530

    feat: render bookmarks in UI

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -15,6 +16,7 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
 
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1 Load Recipe
     await model.loadRecipe(id);
@@ -63,10 +65,15 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
 
+  // update
   recipeView.update(model.state.recipe);
+
+  // render bookmarks
+  bookmarksView(model.state.bookmarks);
 };
 
 const init = function () {
