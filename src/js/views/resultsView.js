e5110708e0c4c3ad69d48f3df8410f7857499bb4
Tanish Jain <tanishj52@gmail.com>
2026-03-24 16:25:50 +0530

    fix: reset pagination to page 1 on new search

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -7,7 +7,6 @@ class ResultsView extends View {
   _successMessage = '';
 
   _generateMarkup() {
-    console.log(this._data);
     return this._data.map(this._generateMarkupPreview).join('');
   }
 
