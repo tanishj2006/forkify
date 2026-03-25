fcdfc644f2bafc8a6e6ab6cfa7366868307f9916
Tanish Jain <tanishj52@gmail.com>
2026-03-25 15:26:16 +0530

    feat: transform recipe form data into API-compatible format

--- src/js/controller.js
+++ src/js/controller.js
@@ -82,8 +82,13 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  console.log(newRecipe);
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    await model.uploadRecipe(newRecipe);
+  } catch (err) {
+    console.error(err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 const init = function () {
